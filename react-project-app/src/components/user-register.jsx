3f5048b3f44997c78fe987525ebd31e8087d8528
srinupraneeth <137474501+srinupraneeth@users.noreply.github.com>
2024-01-25 22:23:54 +0530

    updated
    updated

--- react-project-app/src/components/user-register.jsx
+++ react-project-app/src/components/user-register.jsx
@@ -1,21 +1,63 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useFormik } from "formik";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export function UserRegister(){
+
+    const [users, setusers]= useState([{UserId:'', UserName:'', Password:'', Email:'',  Mobile:''}]);
+    const [usererror,setusererror]= useState('');
+
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:2200/users')
+        .then(responce =>{
+            setusers(responce.data)
+        })
+    },[]);
+
+    let navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues:  {
+            UserId:'',
+            UserName:'',
+            Password:'',
+            Email:'',
+            Mobile:''
+        },
+        onSubmit: (user) => {
+            axios.post('http://127.0.0.1:2200/adduser', user);
+            alert('Registered Successfully..');
+            navigate('/UserLogin');
+        }
+    })
+
+    function VerifyUser(e){
+        for (var user of users){
+            if (user.UserId===e.target.value){
+                setusererror("user id taken - try another");
+                break;
+            }else{
+                setusererror("user id available")
+            }
+        }
+    }
     return(
         <div>
             <h4>register user</h4>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <dl>
                     <dt>User Id</dt>
-                    <dd><input type="text" /></dd>
+                    <dd><input type="text" onKeyUp={VerifyUser} name="UserId" onChange={formik.handleChange} /></dd>
+                    <dd>{usererror}</dd>
                     <dt>User Name</dt>
-                    <dd><input type="text" /></dd>
+                    <dd><input type="text" name="UserName" onChange={formik.handleChange} /></dd>
                     <dt>Password</dt>
-                    <dd><input type="password" /></dd>
+                    <dd><input type="password" name="Password" onChange={formik.handleChange} /></dd>
                     <dt>Email</dt>
-                    <dd><input type="text" /></dd>
+                    <dd><input type="text" name="Email" onChange={formik.handleChange} /></dd>
                     <dt>Mobile</dt>
-                    <dd><input type="text" /></dd>
+                    <dd><input type="text" name="Mobile" onChange={formik.handleChange} /></dd>
                 </dl>
                 <button className="btn btn-primary">Register</button>
                 <Link to={'/'} className="btn btn-light ms-2">Cancel</Link>
